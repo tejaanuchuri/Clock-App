fbba385a91e603a1c27bac68882b230302dc425b
tejaanuchuri0009 <tejaanuchuri0009@gmail.com>
2020-07-21 18:25:13 +0530

    "convert function to classes"

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-function Clock(props) {
-  return (
-    <div>
-      <h1>Hello world...!</h1>
-      <h2>The time is {props.date.toLocaleTimeString()}.</h2>
-    </div>
-  );
+class Clock extends Component {
+  render(){
+    return (
+      <div>
+         <h1>Hello world...!</h1>
+         <h2>The time is {this.props.date.toLocaleTimeString()}.</h2>
+      </div>
+    );
+  }
 }
-
 
 export default Clock;
